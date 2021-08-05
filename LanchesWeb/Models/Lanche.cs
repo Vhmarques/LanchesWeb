8c8bb946b0099877571c4dc4e1cba7e3d0eebbbc
Victor Marques <vhm.marques@hotmail.com>
2021-08-04 23:49:56 -0300

    Criando as entidades lanche e categoria

---
+++ LanchesWeb/Models/Lanche.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesWeb.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(100)]
+        public string DescricaoCurta { get; set; }
+
+        [StringLength(255)]
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+
+        [StringLength(200)]
+        public string ImageUrl { get; set; }
+
+        [StringLength(200)]
+        public string ImageThumbnailUrl { get; set; }
+        public bool LanchePreferido { get; set; }
+        public bool Estoque { get; set; }
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
